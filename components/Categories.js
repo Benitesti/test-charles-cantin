4fc3e1d0b5051739ea9aa92edf16f8aebb81dae7
Benido <remi.besnie@gmail.com>
2022-09-27 14:42:21 +0200

    Add active state to categories button

--- components/Categories.js
+++ components/Categories.js
@@ -1,17 +1,24 @@
+import CategoryButton from "./CategoryButton"
+
 import styles from "./Categories.module.css"
 
 const Categories = ({ handleCategories, handleReset}) => {
 
+// const handleClick = (event) => {
+//   handleCategories(event)
+//   event.target.className.includes("active") ? event.target.className -= "active" : event.target.className += "active"
+//   console.log(event.target.className)
+// }
 
 return (
-  <div className={styles.Categories}>
-    <button onClick={event => handleCategories(event)} value="mariage">Mariage</button>
-    <button onClick={event => handleCategories(event)} value="grossesse">Grossesse</button>
-    <button onClick={event => handleCategories(event)} value="bebe">Bébé</button>
-    <button onClick={event => handleCategories(event)} value="bapteme">Baptême</button>
-    <button onClick={event => handleCategories(event)} value="famille">Famille</button>
-    <button onClick={event => handleCategories(event)} value="portrait">Portrait</button>
-    <button onClick={handleReset}>Toutes</button> 
+  <div >
+    <CategoryButton onClick={handleCategories} value="mariage">Mariage</CategoryButton>
+    <CategoryButton onClick={handleCategories} value="grossesse">Grossesse</CategoryButton>
+    <CategoryButton onClick={handleCategories} value="bebe">Bébé</CategoryButton>
+    <CategoryButton onClick={handleCategories} value="bapteme">Baptême</CategoryButton>
+    <CategoryButton onClick={handleCategories} value="famille">Famille</CategoryButton>
+    <CategoryButton onClick={handleCategories} value="portrait">Portrait</CategoryButton>
+    <CategoryButton onClick={handleReset}>Toutes</CategoryButton> 
   </div>
 )}
 
--- content/gallerie.md
+++ content/gallerie.md
@@ -21,5 +21,23 @@ pictures:
     name: portrait-1
     categories:
       - portrait
+  - photo: images/uploads/family-nathan-dumlao-4_mj1tbmk8a-unsplash.jpg
+    name: family-2
+    categories:
+      - famille
+  - photo: images/uploads/baby-joshua-reddekopp-qlfjkfbpppm-unsplash.jpg
+    name: bebe-2
+    categories:
+      - bebe
+  - photo: images/uploads/baby-joshua-reddekopp-qlfjkfbpppm-unsplash.jpg
+    name: bapteme-1
+    categories:
+      - bapteme
+      - bebe
+  - photo: images/uploads/baptism-547222_1920.jpg
+    name: bapteme-2
+    categories:
+      - bapteme
+      - bebe
 ---
 Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.
