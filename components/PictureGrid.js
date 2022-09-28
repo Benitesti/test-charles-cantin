e26f8fbc56e1510cdbc4e23267de07d91f42df9a
Benido <remi.besnie@gmail.com>
2022-09-28 21:52:37 +0200

    Add an expand effect with blurred background on pictures in gallery

--- components/PictureGrid.js
+++ components/PictureGrid.js
@@ -1,23 +1,42 @@
+import { useState } from "react"
+import { motion } from "framer-motion"
 
-import Image from "next/image"
+import GridImageContainer from "./GridImageContainer"
 
 import styles from "./PictureGrid.module.css"
 
-const PictureGrid = ({ displayedPictures }) => (
-  <div className={styles.PicturesGrid}>
-      {displayedPictures.map((img, i) => (
-        <div className={styles.imgContainer} key={i}>
-          <Image 
-            className={styles.Image} 
-            src= {'/' + img.photo} 
-            layout="fill" 
-            objectFit="cover"
-            sizes="(max-width: 384px) 50vw,
-                  (max-width: 600px) 25vw,
-                  33vw" />
-        </div>  
-      ))}
+
+
+const PictureGrid = ({ displayedPictures }) => {
+
+  const [isBlurred, setIsBlurred] = useState(false)
+  const [isOutsideClick, setIsOutsideClick] = useState(false)
+  
+  const handleClick = () => {
+    setIsBlurred(!isBlurred)}
+  
+
+return (
+  <div className={styles.container}>
+    <div className={styles.grid}>
+        {displayedPictures.map((img, i) => (
+          <GridImageContainer 
+            key={i} 
+            img={img}
+            blur={isBlurred}
+            setBlur={() => setIsBlurred(!isBlurred)} />
+        ))}
     </div>
-)
+    <motion.div 
+      className={styles.blur} 
+      transition={{ duration: 0.5}} 
+      initial={false}
+      animate={{display: isBlurred ? "block" : "none"}}
+      onClick={handleClick}
+      />
+  </div>  
+  )
+}  
+
 
 export default PictureGrid
